38706c79a6b6e4c15669276c14c94346b1deb92d
osamaaAlmahameed <osamaamahameed@gmail.com>
2025-05-23 19:15:12 +0300

    Add ReverseCharacters solution, README, and whiteboard image

--- OneDrive/Desktop/Challenges-and-data-structers/Challenges/Reverse-Characters/ReverseCharacters.js
+++ OneDrive/Desktop/Challenges-and-data-structers/Challenges/Reverse-Characters/ReverseCharacters.js
@@ -1,9 +1,3 @@
-# Reverse Characters
-
-## Challenge Description 
-Write a function `ReverseCharacters` that takes a single string as input. Without using any built-in methods, the function should return the string with all characters reversed.
-
-## Code 
 function ReverseCharacters(characters){  // function called ReverseCharacters accepts paramter
     let reverse = "";          // empty string
     for(let i = characters.length - 1 ; i >= 0 ; i--){   // for loop read from the end and back to start
@@ -16,7 +10,3 @@ function ReverseCharacters(characters){  // function called ReverseCharacters ac
 let word = "Javascript";   // The Word 
 
 console.log(ReverseCharacters(word));   
-
-## Whiteboard
-
-![Whiteboard](./ReverseCharacters.jpg)
